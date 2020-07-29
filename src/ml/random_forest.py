20e428b3666e46d44737e4a802bcf2f3c7930d40
felixvalin <felix.valin@mail.mcgill.ca>
2020-07-29 16:46:59 -0400

    added multiple redshifts to the generation of catalogs and random forest test

--- src/ml/random_forest.py
+++ src/ml/random_forest.py
@@ -4,22 +4,28 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split 
 import pandas as pd
 
+snapshots = [99,91,84,78,72,67,59,50,40,33,25,21]
+results = []
+
+for i, snapshot in enumerate(snapshots):
 # Importing Data
-file_data = pd.read_csv("/data/TNG300-2/catalogs/snap_099-10000_groups.csv", index_col=0)
+     file_data = pd.read_csv("/data/TNG300-2/catalogs/snap_0{}-10000_groups.csv".format(snapshot), index_col=0)
+     
+     # Splitting labels and data
+     # data = file_data.drop(['isBCG'], axis=1)
+     # data = file_data.drop(['isBCG', 'SubhaloGrNr', 'SubhaloGasMetallicity', 'GroupFirstSub', 'GroupNsubs', 'Rel_R'], axis=1)
+     # data = file_data.drop(['isBCG', 'SubhaloGrNr', 'SubhaloGasMetallicity', 'GroupFirstSub', 'GroupNsubs'], axis=1)
+     data = file_data.drop(['isBCG', 'SubhaloGrNr', 'GroupFirstSub', 'GroupNsubs'], axis=1)
 
-# Splitting labels and data
-data = file_data.drop(columns=['isBCG', 'GroupFirstSub', 'GroupNsubs',
-                               'SubhaloGasMetallicity', 'Rel_R',
-                               'SubhaloGrNr', ''])
-labels = file_data['isBCG']
-
-# Splitting into training sets and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data, labels,
-                                                    test_size=.25)
-# Generate the classificator
-clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
-
-# Test the accuracy of the classificator
-print(clf.score(X_test, y_test))
-
+     labels = file_data['isBCG']
+     
+     # Splitting into training sets and testing sets
+     X_train, X_test, y_train, y_test = train_test_split(data, labels,
+                                                         test_size=.25)
+     # Generate the classificator
+     clf = RandomForestClassifier()
+     clf.fit(X_train, y_train)
+     
+     # Test the accuracy of the classificator
+     results.append(clf.score(X_test, y_test))
+     print("Accuracy result for snapshot {}: {}".format(snapshot, results[i]))
